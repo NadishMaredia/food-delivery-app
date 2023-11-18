aa2d256ca7fb8777e6c907a4fd91c55b8a2cd666
Nadish Maredia <nadishns97@gmail.com>
2023-11-18 16:17:42 -0500

    Setup mongodb

--- api/index.js
+++ api/index.js
@@ -1,1 +1,30 @@
-console.log('Hello World')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, './config.env') })
+
+const app = express();
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Express server!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB connection disconnected through app termination');
+    process.exit(0);
+  });
+});
